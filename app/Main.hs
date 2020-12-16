c511f0ec42543b265cf2de9cd610944ed6dd1a54
Joosep Jääger <joosep.jaager@gmail.com>
2020-12-17 00:59:10 +0200

    Added data

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
@@ -8,32 +9,78 @@ import Control.Monad.State
 import Data.Text
 import qualified Data.Map as M
 
-data Disease = Disease
-  { _dBaseInfectionRate :: Float
-  , _dIncubationPeriod  :: Float
-  , _dDuration          :: Float
-  }
-makeLenses ''Disease
-
 data Country = Country
-  { _cName         :: Text
-  , _cSuceptibles  :: Int
-  , _cExposed      :: Int
-  , _cInfectious   :: Int
-  , _cRecovered    :: Int
+  { _cName           :: Text
+  , _cSuceptibles    :: Int
+  , _cExposed        :: Int
+  , _cInfectious     :: Int
+  , _cRecovered      :: Int
+  , _cYearlyTourists :: Int
   }
 makeLenses ''Country
 
 data SimulationState = SimulationState
-  { _ssCountries    :: [Country]
-  , _ssDisease      :: Disease
-  , _ssTravelMatrix :: M.Map (Text, Text) Float
+  { _ssCountries         :: [Country]
+  , _ssBaseInfectionRate :: Float
+  , _ssIncubationPeriod  :: Float
+  , _ssDiseaseDuration   :: Float
+  , _ssTravelMatrix      :: M.Map (Text, Text) Float
   }
 makeLenses ''SimulationState
 
 main :: IO ()
 main = 
   do
+    let countries =
+          [ Country
+              { _cName = "Eesti"
+              , _cSuceptibles = 1328976
+              , _cExposed = 0
+              , _cInfectious = 0
+              , _cRecovered = 0
+              , _cYearlyTourists = 3789955
+              }
+          , Country
+              { _cName = "Läti"
+              , _cSuceptibles = 2795000
+              , _cExposed = 0
+              , _cInfectious = 0
+              , _cRecovered = 0
+              , _cYearlyTourists = undefined
+              }
+          , Country
+              { _cName = "Soome"
+              , _cSuceptibles = 5518000
+              , _cExposed = 0
+              , _cInfectious = 0
+              , _cRecovered = 0
+              , _cYearlyTourists = undefined
+              }
+          , Country
+              { _cName = "Venemaa"
+              , _cSuceptibles = 144500000
+              , _cExposed = 0
+              , _cInfectious = 0
+              , _cRecovered = 0
+              , _cYearlyTourists = undefined
+              }
+          ]
+    let travelMatrix = M.fromList
+          -- Overnight stays
+          [ (("Läti", "Eesti"), 182860)
+          , (("Soome", "Eesti"), 804645)
+          , (("Venemaa", "Eesti"), 260036)
+          , (("Eesti", "Läti"), 210300)
+          , (("Soome", "Läti"), 82500)
+          , (("Venemaa", "Läti"), 297800)
+          , (("Läti", "Soome"), 50000)
+          , (("Eesti", "Soome"), 243000)
+          , (("Venemaa", "Soome"), 814600)
+          -- Total visitors
+          , (("Eesti", "Venemaa"), 540062)
+          , (("Läti", "Venemaa"), 365783)
+          , (("Soome", "Venemaa"), 938693)
+          ]
     undefined
 
 update :: State SimulationState ()
@@ -53,9 +100,9 @@ update =
 updateInternal :: Country -> State SimulationState Country
 updateInternal country =
   do
-    infectionRate    <- use $ ssDisease . dBaseInfectionRate
-    incubationPeriod <- use $ ssDisease . dBaseInfectionRate
-    duration         <- use $ ssDisease . dDuration
+    infectionRate    <- use ssBaseInfectionRate
+    incubationPeriod <- use ssBaseInfectionRate
+    duration         <- use ssDiseaseDuration
 
     let suceptibles = fromIntegral $ country ^. cSuceptibles
     let exposed     = fromIntegral $ country ^. cExposed
